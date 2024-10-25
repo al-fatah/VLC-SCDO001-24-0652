e62f4dda65621b94d08b2e08cabf5b373d424515
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-25 18:31:01 +0530

    add import modules

---
+++ functions/use_colors.py
@@ -0,0 +1,22 @@
+#  lamda functions
+
+RESET = "\033[0m"
+BLACK = "\033[30m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+MAGENTA = "\033[35m"
+CYAN = "\033[36m"
+WHITE = "\033[37m"
+
+get_red = lambda s: print(RED + s +RESET)
+get_green = lambda s: print(GREEN + s +RESET)
+get_yellow = lambda s: print(YELLOW + s +RESET)
+get_blue = lambda s: print(BLUE + s +RESET)
+get_magenta = lambda s: print(MAGENTA + s +RESET)
+get_cyan = lambda s: print(CYAN + s +RESET)
+get_white = lambda s: print(WHITE + s +RESET)
+
+
+
