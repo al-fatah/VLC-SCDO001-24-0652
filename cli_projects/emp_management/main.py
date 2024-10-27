e9bc91858bcd2adeb5812279abe6fe73e26d9631
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-27 10:00:46 +0530

    add try except to project

--- cli_projects/emp_management/main.py
+++ cli_projects/emp_management/main.py
@@ -21,12 +21,21 @@ def main():
         option = int(input("Select a choice from the Menu "))
 
         if option == 1:
-            emp_id = int(input("Enter Employee ID: "))
+            while True:
+                try:
+                    emp_id = int(input("Enter Employee ID: "))
+                    break
+                except ValueError:
+                    print("Enter a number")
             emp_name = input("Enter Employee Name: ")
             emp_dept = input("Enter the Department: ")
             new_employee.create_employee(emp_name,emp_id,emp_dept)
         elif option == 2:
-            emp_id = int(input("Enter the Employee's id to Update: "))
+            while True:
+                try:
+                    emp_id = int(input("Enter the Employee's id to Update: "))
+                except ValueError:
+                    print("Enter a number")
             if new_employee.emp_present(emp_id):
                 emp_name = input("Enter the New Employee Name: ")
                 emp_dept = input("Enter the New Department: ")
@@ -34,10 +43,20 @@ def main():
             else:
                 print("Employee Not found")
         elif option == 3:
-            emp_id = int(input("Enter the Employee's id to Delete: "))
+            while True:
+                try:
+                    emp_id = int(input("Enter the Employee's id to Delete: "))
+                except ValueError:
+                    print("Enter a number")
+
             new_employee.delete_employee(emp_id)
         elif option == 4:
-            emp_id = int(input("Enter the Employee's id: "))
+            while True:
+                try:
+                    emp_id = int(input("Enter the Employee's id: "))
+                except ValueError:
+                    print("Enter a number")
+
             e = new_employee.show_employee(emp_id)
             if e:
                 print(e)
@@ -47,6 +66,8 @@ def main():
         elif option == 6:
             print("Thanks for Using the System...........")
             break
+        else:
+            print("Invalid Option, please select a right one") 
 
 if __name__ == "__main__":
     main()
